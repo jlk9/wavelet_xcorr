1eed8a42839c5ac148cd25fda60c988458c6ce36
Joseph Kump <josek97@vt.edu>
2021-06-24 23:10:18 -0400

    Fixed thresholding method

--- test_cases/basic_test_case.py
+++ test_cases/basic_test_case.py
@@ -8,22 +8,24 @@ import h5py
 import numpy as np
 import pywt
 import time
+import matplotlib.pyplot as plt
 
 from wavelet_xcorr.support_code.generate_all_weights_functions import generate_stacked_weight_matrices
+from wavelet_xcorr.support_code.thresholding_functions         import threshold_coeffs_one_channel, make_sparse_coeffs
 from wavelet_xcorr.dense_code.calculate_nlevel_xcorrs          import calculate_nlevel_xcorrs
 from wavelet_xcorr.sparse_code.calculate_nlevel_xcorrs_sparse  import calculate_nlevel_xcorrs_sparse
 
 """ Now that we have a correct compute_all_diagonals sparse implementation, we'll try plugging it into our
-	wavelet domain xcorr algorithm and evaluating.
+    wavelet domain xcorr algorithm and evaluating.
 
-	After that, we'll incorporate sparsity into the beginning and end terms. The easiest way to do that is
-	probably by just using numpy searchsorted and indexing to correctly form the begin and end data matrices,
-	and then just use normal dense operations. (We can try sparse matrix operations there if it's too slow).
+    After that, we'll incorporate sparsity into the beginning and end terms. The easiest way to do that is
+    probably by just using numpy searchsorted and indexing to correctly form the begin and end data matrices,
+    and then just use normal dense operations. (We can try sparse matrix operations there if it's too slow).
 
 """
 
 wavelet = "db3"
-level   = 3
+level   = 4
 mode    = "periodic"
 
 # Here we'll test our timing and accuracy on an autocorrelation:
@@ -41,36 +43,41 @@ noise_mean = 0
 noise_std  = 2000
 signal1   += np.random.normal(noise_mean, noise_std, len(signal1))
 
-signal2 = signal1[:15000]
+#signal2 = signal1[200:15200]
+signal1 = signal1[:3008]
+signal2 = signal1[200:1704]
 
 coeffs1 = pywt.wavedec(signal1, wavelet, level=level, mode=mode)
 coeffs2 = pywt.wavedec(signal2, wavelet, level=level, mode=mode)
-lagmax  = 1000
+lagmax  = 1008
 
 # First, we need to threshold our wavelet functions and create their sparse representations.
 # For now, we're thresholding each level separately.
-threshold      = 90
-thresh_coeffs1 = []
-thresh_coeffs2 = []
-sparse_coeffs1 = []
-sparse_coeffs2 = []
-for i in range(len(coeffs1)):
-	thresh_coeffs1.append(pywt.threshold(coeffs1[i], np.percentile(np.abs(coeffs1[i]), threshold), mode='soft'))
-	thresh_coeffs2.append(pywt.threshold(coeffs2[i], np.percentile(np.abs(coeffs1[i]), threshold), mode='soft'))
-
-	sparse_coeffs1.append((np.nonzero(thresh_coeffs1[i])[0], thresh_coeffs1[i][np.nonzero(thresh_coeffs1[i])[0]], len(thresh_coeffs1[i])))
-	sparse_coeffs2.append((np.nonzero(thresh_coeffs2[i])[0], thresh_coeffs2[i][np.nonzero(thresh_coeffs2[i])[0]], len(thresh_coeffs2[i])))
+threshold      = 80
+thresh_coeffs1 = threshold_coeffs_one_channel(coeffs1, threshold)
+thresh_coeffs2 = threshold_coeffs_one_channel(coeffs2, threshold)
+sparse_coeffs1 = make_sparse_coeffs(thresh_coeffs1)
+sparse_coeffs2 = make_sparse_coeffs(thresh_coeffs2)
 
 
 # Let's just check and make sure our weight matrices are reorganized properly:
 stacked_weight_matrices, stacked_mixed_weight_matrices, mixed_endpoint_indices = generate_stacked_weight_matrices(wavelet, level)
 
-dense_xcorrs  = calculate_nlevel_xcorrs(stacked_weight_matrices, stacked_mixed_weight_matrices,
-                                        mixed_endpoint_indices, thresh_coeffs1, thresh_coeffs2, lagmax)
+time_xcorrs   = np.correlate(signal1[:len(signal2)+lagmax-1], signal2, mode="valid")
+
+#dense_xcorrs  = calculate_nlevel_xcorrs(stacked_weight_matrices, stacked_mixed_weight_matrices,
+#                                        mixed_endpoint_indices, thresh_coeffs1, thresh_coeffs2, lagmax)
+
 sparse_xcorrs = calculate_nlevel_xcorrs_sparse(stacked_weight_matrices, stacked_mixed_weight_matrices,
                                                mixed_endpoint_indices, sparse_coeffs1, sparse_coeffs2, lagmax)
 
-relative_errors = np.abs(sparse_xcorrs - dense_xcorrs) / (np.abs(dense_xcorrs) + 1)
+relative_errors = np.abs(sparse_xcorrs - time_xcorrs) / (np.abs(time_xcorrs) + 1)
+
+plt.figure(figsize=(16,6))
+plt.plot(time_xcorrs)
+plt.plot(sparse_xcorrs)
+plt.savefig("xcorrs.pdf", format="pdf")
+plt.show()
 
 
 print(np.max(relative_errors))
@@ -84,18 +91,18 @@ dense_times = np.zeros(100)
 sparse_times = np.zeros(100)
 
 for i in range(samples):
-	t0 = time.time()
-	dense_xcorrs = calculate_nlevel_xcorrs(stacked_weight_matrices, stacked_mixed_weight_matrices,
-                                           mixed_endpoint_indices, thresh_coeffs1, thresh_coeffs2, lagmax)
-	t1 = time.time()
-	dense_times[i] = t1 - t0
+    t0 = time.time()
+    #dense_xcorrs = calculate_nlevel_xcorrs(stacked_weight_matrices, stacked_mixed_weight_matrices,
+    #                                       mixed_endpoint_indices, thresh_coeffs1, thresh_coeffs2, lagmax)
+    t1 = time.time()
+    dense_times[i] = t1 - t0
 
 for i in range(samples):
-	t0 = time.time()
-	sparse_xcorrs = calculate_nlevel_xcorrs_sparse(stacked_weight_matrices, stacked_mixed_weight_matrices,
+    t0 = time.time()
+    sparse_xcorrs = calculate_nlevel_xcorrs_sparse(stacked_weight_matrices, stacked_mixed_weight_matrices,
                                                    mixed_endpoint_indices, sparse_coeffs1, sparse_coeffs2, lagmax)
-	t1 = time.time()
-	sparse_times[i] = t1 - t0
+    t1 = time.time()
+    sparse_times[i] = t1 - t0
 
 print(np.mean(dense_times), np.mean(sparse_times))
 
